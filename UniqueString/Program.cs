78c8913079e072114090e4ae9864555ab5ab208f
mou-inoks <mou@inokscapital.com>
2024-01-10 22:13:47 +0100

    first commit

---
+++ UniqueString/Program.cs
@@ -0,0 +1,18 @@
+﻿//Program to verify by an algorithm if element contains double
+
+bool Test(string word)
+{
+    var isUnique = false;
+    
+    for (int i = 0; i < word.Length; i++)
+    {
+        for (int j = 0; j < word.Length; j++)
+        {
+            if (word[i] == word[j] && i != j)
+                isUnique = true;
+        }
+    }
+    return isUnique;
+}
+
+Console.WriteLine(Test("test"));
